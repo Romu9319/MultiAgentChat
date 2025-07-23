200dfe5675f6712a58b2e003a888e9142618cb21
unknown <danielerm1510@gmail.com>
2025-07-23 21:51:42 +0200

    simulate agents list added

--- backend/app/routers/agents.py
+++ backend/app/routers/agents.py
@@ -3,14 +3,24 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
+AGENTS = ["Agente1", "Agente2", "Verificador", "Planificador"]
+
 class AgentInput(BaseModel):
     name: str
     prompt: str
 
 
+@router.get("/agents", response_model = list[str])
+def list_agents():
+    return AGENTS
+
+
 @router.post("/agent/respond")
 def get_agent_response(data: AgentInput):
     try:
+        if data.name not in AGENTS:
+            raise HTTPException(status_code=404, detail= f"Agente {data.name} no encontrado")
+
         if not data.prompt.strip():
             raise HTTPException(status_code=400, detail="Necesita insertar algun prompt")
         
