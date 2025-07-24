0dffa31aedefc1161f63345ce7409e442c4a7a3f
unknown <danielerm1510@gmail.com>
2025-07-24 22:23:13 +0200

    CRUD for agents added

--- backend/app/routers/agents.py
+++ backend/app/routers/agents.py
@@ -1,27 +1,53 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 router = APIRouter()
 
-AGENTS = ["Agente1", "Agente2", "Verificador", "Planificador"]
+AGENTS : List[str] = ["Agente1", "Agente2", "Verificador", "Planificador"]
 
 class AgentInput(BaseModel):
     name: str
+
+class AgentMessageInput(BaseModel):
+    name: str 
     prompt: str
 
 
-@router.get("/agents", response_model = list[str])
+@router.get("/agents", response_model = List[str])
 def list_agents():
     return AGENTS
 
 
+@router.post("/agents", status_code=201)
+def create_agent(data: AgentInput):
+    name = data.name.strip()
+    if not name:
+        raise HTTPException(status_code=400, detail="El nombre del agente no puede estar vacio")
+    if name in AGENTS:
+        raise HTTPException(status_code=409, detail=f"El Agente '{name}' ya existe")
+    AGENTS.append(name)
+    return {"detail": f"Agente '{name}' creado"}
+
+
+@router.delete("/agentes/{name}")
+def delete_agent(name:str):
+    if name not in AGENTS:
+        raise HTTPException(status=404, detail="El agente '{name}' no existe")
+    AGENTS.remove(name)
+    return {"detail": f"Agente '{name}' eliminado"}
+
+
 @router.post("/agent/respond")
 def get_agent_response(data: AgentInput):
+    name = data.name.strip()
+    prompt = data.prompt.strip()
+
     try:
-        if data.name not in AGENTS:
+        if name not in AGENTS:
             raise HTTPException(status_code=404, detail= f"Agente {data.name} no encontrado")
 
-        if not data.prompt.strip():
+        if not prompt:
             raise HTTPException(status_code=400, detail="Necesita insertar algun prompt")
         
         response = f"[Simulacion] El agente '{data.name} responde a: '{data.prompt}"
